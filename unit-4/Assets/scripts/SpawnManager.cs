a9b99bd3ef1dcda93523328bb16292603f4a6bdf
MSI\roywi <roywilliamvermeulen@gmail.com>
2021-05-21 10:43:12 +0200

    kwam niet uit unit 4 had iets the maken met het woor lenght dat niet werkte. de challenge is wel gelukt

---
+++ unit-4/Assets/scripts/SpawnManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    private float spawnRange = 9.0f;
+    public GameObject enemyPrefab;
+    public int enemyCount;
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnEnemyWave(3);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (enemyCount == 0)
+        {
+            SpawnEnemyWave(1);
+        }
+    }
+    private Vector3 GenerateSpawnPosition()
+    {
+        float spawnPosX = Random.Range(spawnRange, spawnRange);
+        float spawnPosZ = Random.Range(spawnRange, spawnRange);
+        Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
+        return randomPos;
+    }
+    void SpawnEnemyWave(int enemiesToSpawn)
+    {
+        for (int i = 0; i < enemiesToSpawn; i++)
+        {
+            Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+        }
+
+    }
+
+}
