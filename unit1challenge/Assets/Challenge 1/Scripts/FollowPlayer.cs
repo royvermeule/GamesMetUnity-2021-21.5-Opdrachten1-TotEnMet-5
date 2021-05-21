8f3e91babf2515410ce7d9c1244878e7e2de06be
MSI\roywi <roywilliamvermeulen@gmail.com>
2021-05-21 07:21:34 +0200

    bij dit unit heb ik hem er niet bij inbegrepen

---
+++ unit1challenge/Assets/Challenge 1/Scripts/FollowPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject Player;
+    public Vector3 offset = new Vector3(30, 0, 10);
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - Player.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Offset the camare beside the plane by adding to the plane's possition
+        transform.position = Player.transform.position + offset;
+    }
+}
